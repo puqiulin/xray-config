a31a8eea32f1f846f4794d407103585509ceac20
one0day <puqiulin20@gmail.com>
2024-01-10 09:48:17 +0800

    feat: init

--- generate.sh
+++ generate.sh
@@ -6,14 +6,12 @@ if [[ -z "$XRAY_MAC_UUID" || -z "$XRAY_IPHONE_UUID" ]]; then
 fi
 
 client_template=$(cat client_template.json)
-output=${template//"%XRAY_MAC_UUID%"/$XRAY_MAC_UUID}
-echo "$output" > client.json
-echo "client.json has been generated"
+output=${client_template//"%XRAY_MAC_UUID%"/$XRAY_MAC_UUID}
+echo "$output" > client.json && echo "client.json generated"
 
 server_template=$(cat server_template.json)
 output=${server_template//"%XRAY_MAC_UUID%"/$XRAY_MAC_UUID}
 output=${output//"%XRAY_IPHONE_UUID%"/$XRAY_IPHONE_UUID}
-echo "$output" > server.json
-echo "server.json has been generated"
+echo "$output" > server.json && echo "server.json generated"
 
 cp server.json /usr/local/etc/xray/config.json && echo "cp server.json to /usr/local/etc/xray/config.json success"
